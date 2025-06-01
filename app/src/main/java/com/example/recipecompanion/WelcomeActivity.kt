2c2022d0b88c8fbb4031de03717fe6c2f44292ff
Kofi Kwakye <kofi.kwakye@live.com>
2025-06-01 12:32:48 +0100

    Fixed bugs and test app launch

--- app/src/main/java/com/example/recipecompanion/WelcomeActivity.kt
+++ app/src/main/java/com/example/recipecompanion/WelcomeActivity.kt
@@ -1,19 +1,18 @@
 package com.example.recipecompanion
 
+import android.app.Activity
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 
 /**
- * Welcome screen activity - displays app branding and entry point
- * Placeholder class for Coursework 1 - functionality to be implemented in Coursework 2
+ * Welcome Activity - Splash Screen (Empty placeholder for Coursework 1)
+ * Functionality will be implemented in Coursework 2
  */
-class WelcomeActivity : AppCompatActivity() {
+class WelcomeActivity : Activity() {  // Changed from AppCompatActivity to Activity
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
 
-        // TODO: Implement GET STARTED button click listener in Coursework 2
-        // TODO: Navigate to RecipeDashboardActivity when button is clicked
+        // TODO: Implement navigation functionality in Coursework 2
     }
 }
